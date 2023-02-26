6e51f3634c34ce992eede3a217582580626b81c8
Hanna <anna.kozyreva.w@gmail.com>
2023-02-27 00:51:23 +0300

    textures description removed from files

--- qa_guru_python_4_2_search.py
+++ qa_guru_python_4_2_search.py
@@ -3,18 +3,6 @@ from selene import be, have
 import pytest
 from selenium import webdriver
 
-@pytest.fixture()
-def browser_size_1280_920():
-    browser.config.window_width = 1280
-    browser.config.window_height = 920
-
-@pytest.fixture()
-def browser_size_1680_1050():
-    browser.config.window_width = 1680
-    browser.config.window_height = 1050
-    yield browser_size_1680_1050
-    browser.config.hold_browser_open = True
-
 def test_search_1(browser_size_1280_920):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
